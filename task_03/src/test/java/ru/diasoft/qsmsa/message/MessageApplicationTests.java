20f4df7559597c095e5c5cc99b24216dbecf4c0c
asosnin <asosnin@diasoft.ru>
2021-08-02 14:23:45 +0300

    task_03 add application

---
+++ task_03/src/test/java/ru/diasoft/qsmsa/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.qsmsa.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
